2a2c8f497fda47109fa2ecc00c6c24ac448209ee
Kofi Kwarteng <info@artheontech.com>
2015-03-24 14:26:09 -0500

    video-hotspot

--- index.php
+++ index.php
@@ -103,7 +103,7 @@
             <div class="col-md-6 col-md-offset-3">
 <div class="video_holder">
                <video id="video1" class="video-js vjs-default-skin vjs-big-play-centered video-js-fullscreen" width="600" height="324" controls preload="none" autoplay preload="none" data-setup='{ "example_option": true, "width": "auto", "height": "auto", "techOrder": ["html5","flash"] }'>
-    <source src="http://hotspot.achisco.com/images/<?php echo $video; ?>" type='video/mp4' />
+    <source src="http://example.com/videos/<?php echo $video; ?>" type='video/mp4' />
   </video> 
                
                </div> 
